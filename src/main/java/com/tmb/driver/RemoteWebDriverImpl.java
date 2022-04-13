67f1cee0322606b53253980f76461eb13ad906c6
williamrichaard <wrichaard@hotmail.com>
2022-04-12 23:00:16 -0300

    added Driver Manager

---
+++ src/main/java/com/tmb/driver/RemoteWebDriverImpl.java
@@ -0,0 +1,15 @@
+package com.tmb.driver;
+
+import com.tmb.driver.factory.web.local.LocalDriverFactory;
+import com.tmb.driver.factory.web.remote.RemoteDriverFactory;
+import com.tmb.driver.intity.WebDriverData;
+import org.openqa.selenium.WebDriver;
+
+public class RemoteWebDriverImpl implements IWebDriver{
+
+    @Override
+    public WebDriver getDriver(WebDriverData driverData) {
+        return RemoteDriverFactory.getDriver(driverData.getBrowserRemoteModeType()
+                ,driverData.getBrowserType());
+    }
+}

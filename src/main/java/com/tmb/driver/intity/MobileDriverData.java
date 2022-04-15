a09f1ed777b8d297a8b97736f848721929f32554
williamrichaard <wrichaard@hotmail.com>
2022-04-14 23:07:00 -0300

    added Driver Manager

--- src/main/java/com/tmb/driver/intity/MobileDriverData.java
+++ src/main/java/com/tmb/driver/intity/MobileDriverData.java
@@ -2,15 +2,13 @@ package com.tmb.driver.intity;
 
 import com.tmb.enums.MobilePlatformType;
 import com.tmb.enums.MobileRemoteModeType;
-import com.tmb.enums.RunModeType;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Builder
+@AllArgsConstructor
 @Getter
 public class MobileDriverData {
 
     private MobilePlatformType mobilePlatformType;
     private MobileRemoteModeType mobileRemoteModeType;
-    private RunModeType runModeType;
 }

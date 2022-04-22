065a13e1093b168694d7b7888df77ec064ea20bd
williamrichaard <wrichaard@hotmail.com>
2022-04-22 14:15:47 -0300

    enabled parallel execution and context switching

--- src/main/java/com/tmb/driver/impl/web/LocalWebDriverImpl.java
+++ src/main/java/com/tmb/driver/impl/web/LocalWebDriverImpl.java
@@ -1,10 +1,11 @@
-package com.tmb.driver;
+package com.tmb.driver.impl.web;
 
+import com.tmb.driver.IWebDriver;
 import com.tmb.driver.factory.web.local.LocalDriverFactory;
 import com.tmb.driver.intity.WebDriverData;
 import org.openqa.selenium.WebDriver;
 
-public class LocalWebDriverImpl implements IWebDriver{
+public class LocalWebDriverImpl implements IWebDriver {
 
     @Override
     public WebDriver getDriver(WebDriverData driverData) {

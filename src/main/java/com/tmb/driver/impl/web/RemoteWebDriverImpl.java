34e05a7b86a89b44157922a1bb5086994a9fd48e
williamrichaard <wrichaard@hotmail.com>
2022-04-22 14:15:50 -0300

    enabled parallel execution and context switching

--- src/main/java/com/tmb/driver/impl/web/RemoteWebDriverImpl.java
+++ src/main/java/com/tmb/driver/impl/web/RemoteWebDriverImpl.java
@@ -1,11 +1,12 @@
-package com.tmb.driver;
+package com.tmb.driver.impl.web;
 
+import com.tmb.driver.IWebDriver;
 import com.tmb.driver.factory.web.local.LocalDriverFactory;
 import com.tmb.driver.factory.web.remote.RemoteDriverFactory;
 import com.tmb.driver.intity.WebDriverData;
 import org.openqa.selenium.WebDriver;
 
-public class RemoteWebDriverImpl implements IWebDriver{
+public class RemoteWebDriverImpl implements IWebDriver {
 
     @Override
     public WebDriver getDriver(WebDriverData driverData) {
